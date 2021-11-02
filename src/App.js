8c02638c776932dc009559b76b3a871cfef0eb40
Inokcode <regilearn@gmail.com>
2021-11-02 05:50:45 +0530

    use useState method

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import FormInput from './components/FormInput';
 
 function App() {
+  const [userName, setUserName] = useState('');
+  console.log(userName);
+  console.log('re-renderd');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form>
+        <FormInput placeholder="Username" setUserName={setUserName} />
+        <FormInput placeholder="Email" />
+        <FormInput placeholder="Full Name" />
+        <FormInput placeholder="Sth else" />
+      </form>
     </div>
   );
 }
